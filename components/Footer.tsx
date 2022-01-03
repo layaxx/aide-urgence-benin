624d0d5177db1157cc8d78b0feeec64804079d93
Yannick Lang <layaxx@gmx.de>
2022-01-03 01:02:46 +0100

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,22 @@
 import { useTranslation } from "next-i18next"
+import Link from "next/link"
+import { SocialIcon } from "react-social-icons"
 
 export default function Footer() {
   const { t } = useTranslation()
 
+  const url = "https://www.instagram.com/aide_urgence_benin/"
+
   return (
     <footer className="container flex-between">
       <small>{t("footer.tagline")}</small>
+
+      <SocialIcon url={url} />
+
       <small>
-        <a>{t("footer.links.imprint")}</a>
+        <Link href={"/imprint"}>
+          <a>{t("footer.links.imprint")}</a>
+        </Link>
       </small>
     </footer>
   )
